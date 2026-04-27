9fedbb3bca7a06eb99270c020a7a8a13e35af201
HuuNhiem <nghiahangbobi@gmail.com>
2026-04-27 08:53:15 +0700

    GoogleService

--- backend/backend/src/main/java/com/example/backend/service/GoogleAuthService.java
+++ backend/backend/src/main/java/com/example/backend/service/GoogleAuthService.java
@@ -3,14 +3,44 @@ import com.example.backend.entity.User;
 import com.example.backend.respository.UserResponsitory;
 import com.example.backend.util.JwtUtil;
 import com.google.api.client.googleapis.auth.oauth2.*;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+
 import java.util.Map;
 
 
 @Service
 public class GoogleAuthService {
+    private final UserResponsitory userRepository;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public GoogleAuthService(UserResponsitory userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String authenticate(String idToken) {
+
+        String url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + idToken;
+
+        Map response = restTemplate.getForObject(url, Map.class);
+
+        if (response == null || response.get("email") == null) {
+            throw new RuntimeException("Invalid Google token");
+        }
+
+        String email = (String) response.get("email");
+        String name = (String) response.get("name");
+
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            user = new User();
+            user.setEmail(email);
+            user.setFull_name(name);
+            user = userRepository.save(user);
+        }
+
+        return JwtUtil.generateToken(user.getEmail());
+    }
 }
